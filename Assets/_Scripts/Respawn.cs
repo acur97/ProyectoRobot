bf75c10bdc3f16301c31c4215222dd2312b3b4be
thunderbol <jdvid.bermudez@gmail.com>
2019-02-28 16:31:17 -0500

    Button&JoysticsControl

---
+++ Assets/_Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public GameObject respawn;
+
+
+
+    private void OnTriggerEnter(Collider ColliderBullet)
+    {
+     
+        if(ColliderBullet.transform.tag == "Player")
+        {
+            this.transform.position = respawn.transform.position;
+        }
+           
+    }
+
+}
