c19dc0a7e3412c9fbbeebbafca44610eb74095c9
Andres Urrego <acur97.tk@gmail.com>
2019-02-28 10:12:16 -0500

    base2

---
+++ Assets/_Scripts/enumTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enumTest : MonoBehaviour
+{
+    public enum jugadores {Jugador1, Jugador2, Jugador3}
+    public jugadores jug;
+
+    private string salto1 = "Jump1";
+    private string salto2 = "Jump2";
+    private string salto;
+
+    private void Awake()
+    {
+        if (jugadores.Jugador1 == jug)
+        {
+            salto = salto1;
+        }
+    }
+
+    private void Update()
+    {
+        Input.GetButtonDown(salto);
+    }
+}
