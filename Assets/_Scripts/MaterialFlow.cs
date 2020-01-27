7066b559768abd62a31bebc9191c18c21aad751e
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-01-26 23:07:00 -0500

    no message

---
+++ Assets/_Scripts/MaterialFlow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialFlow : MonoBehaviour
+{
+    public bool activado = true;
+    public float velocidad;
+    public Color col;
+    private float m_Hue = 0;
+    private float m_Saturation = 1;
+    private float m_Value = 1;
+
+    public Material matPiso;
+
+    private void Update()
+    {
+        if (activado)
+        {
+            m_Hue += Time.unscaledDeltaTime * velocidad;
+            if (m_Hue > 1)
+            {
+                m_Hue = 0;
+            }
+            col = Color.HSVToRGB(m_Hue, m_Saturation, m_Value, false);
+            matPiso.SetColor("_BaseColor", col);
+        }
+    }
+}
